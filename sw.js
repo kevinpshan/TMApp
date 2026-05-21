128fd99e73176d684be398958eb0b7c127a7dd74
kevinpshan <kevinpshan@gmail.com>
2026-05-21 20:21:16 +0000

    Initial TMApp deploy setup

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'tm-app-v1.0.0';
+const ASSETS = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+    self.skipWaiting();
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+        ).then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
